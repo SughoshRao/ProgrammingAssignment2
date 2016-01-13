8c3baf0bb1e9f131565445da4cf09f813c9f9eac
Sughosh <rao.sughosh@yahoo.in>
2016-01-13 23:21:50 +0530

    File modified as part of Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Program caches the inverse of a matrix to be reused
+## so long as the original matrix is not changed.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix: This function creates a special "matrix" 
+## object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) 
+{
+    inv <- NULL
+    set <- function(y) 
+	{
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve: This function computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above. If the inverse has 
+## already been calculated (and the matrix has not changed), then 
+## the cachesolve retrieves the inverse from the cache.
+cacheSolve <- function(x, ...) 
+{
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+## Sample run:
+## > source("cachematrix.R")
+## creating a new matrix (lets call it 'a')
+## > x <- rbind(c(1, -1/4), c(-1/4, 1))
+## > m <- makeCacheMatrix(x)
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## Calling cacheSolve again with matrix 'a' unchanged
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## creating a new matrix 'b'
+## > x <- rbind(c(1, -1/2), c(-1/2, 1))
+## > m <- makeCacheMatrix(x)
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.3333333 0.6666667
+## [2,] 0.6666667 1.3333333
+## Calling cacheSolve again with matrix 'b' unchanged 
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.3333333 0.6666667
+## [2,] 0.6666667 1.3333333
